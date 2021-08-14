2984f8c0030b7f81b4a7f8b76737a3611c97eff5
Leandro Sales <leandrossales.96@gmail.com>
2021-08-14 20:36:32 -0300

    CRD routes update

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -8,24 +8,53 @@ const router = express.Router();
 
 router.use(authMiddleware);
 
+//list route
 router.get('/', async (req, res) => {
-    res.send({ user: req.userId });
+    try {
+        const projects = await Project.find().populate('user');
+
+        return res.send({ projects });
+    } catch (err) {
+        return res.status(400).send({ error: 'Error loading projects'});
+    }
 });
 
+//show route
 router.get('/:projectID', async (req, res) => {
-    res.send({ user: req.userId });
+    try {
+        const project = await Project.findById(req.params.projectID).populate('user');
+
+        return res.send({ project });
+    } catch (err) {
+        return res.status(400).send({ error: 'Error loading project'});
+    }
 });
 
+//create route
 router.post('/', async (req, res) => {
-    res.send({ user: req.userId });
+    try {
+        const project = await Project.create({ ...req.body, user: req.userId });
+
+        return res.send({ project });
+    } catch (err) {
+        return res.status(400).send({ error: 'Error creating new project'});
+    }
 });
 
+//update route
 router.put('/:projectID', async (req, res) => {
     res.send({ user: req.userId });
 });
 
+//delete route
 router.delete('/:projectID', async (req, res) => {
-    res.send({ user: req.userId });
+    try {
+        await Project.findByIdAndRemove(req.params.projectID);
+
+        return res.send();
+    } catch (err) {
+        return res.status(400).send({ error: 'Error deleting project'});
+    }
 });
 
 module.exports = app => app.use('/projects', router);
