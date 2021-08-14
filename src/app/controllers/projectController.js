cb2f271962fcf5e689f8c36e38419a7f6aaef0d5
Leandro Sales <leandrossales.96@gmail.com>
2021-08-14 18:33:20 -0300

    Create task model and CRUD routes

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const authMiddleware = require ('../middlewares/auth');
 
+const Project = require('../models/project');
+const Task = require('../models/task');
+
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.get('/', (req, res) => {
-    res.send({ ok: true, user: req.userId });
+router.get('/', async (req, res) => {
+    res.send({ user: req.userId });
+});
+
+router.get('/:projectID', async (req, res) => {
+    res.send({ user: req.userId });
+});
+
+router.post('/', async (req, res) => {
+    res.send({ user: req.userId });
+});
+
+router.put('/:projectID', async (req, res) => {
+    res.send({ user: req.userId });
+});
+
+router.delete('/:projectID', async (req, res) => {
+    res.send({ user: req.userId });
 });
 
 module.exports = app => app.use('/projects', router);
