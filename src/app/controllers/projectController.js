73f17ba68d4f7aee06c119f9ab667076c868045e
Leandro Sales <leandrossales.96@gmail.com>
2021-08-14 23:30:43 -0300

    att create and update functions

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -11,7 +11,7 @@ router.use(authMiddleware);
 //list route
 router.get('/', async (req, res) => {
     try {
-        const projects = await Project.find().populate('user');
+        const projects = await Project.find().populate(['user', 'tasks']);
 
         return res.send({ projects });
     } catch (err) {
@@ -22,7 +22,7 @@ router.get('/', async (req, res) => {
 //show route
 router.get('/:projectID', async (req, res) => {
     try {
-        const project = await Project.findById(req.params.projectID).populate('user');
+        const project = await Project.findById(req.params.projectID).populate(['user', 'tasks']);
 
         return res.send({ project });
     } catch (err) {
@@ -33,7 +33,19 @@ router.get('/:projectID', async (req, res) => {
 //create route
 router.post('/', async (req, res) => {
     try {
-        const project = await Project.create({ ...req.body, user: req.userId });
+        const { title, description, tasks } = req.body;
+
+        const project = await Project.create({ title, description, user: req.userId });
+
+        await Promise.all(tasks.map(async task => {
+            const projectTask = new Task({ ...task, project: project._id });
+
+            await projectTask.save();
+
+            project.tasks.push(projectTask);
+        }));
+
+        await project.save();
 
         return res.send({ project });
     } catch (err) {
@@ -43,7 +55,31 @@ router.post('/', async (req, res) => {
 
 //update route
 router.put('/:projectID', async (req, res) => {
-    res.send({ user: req.userId });
+    try {
+        const { title, description, tasks } = req.body;
+
+        const project = await Project.findByIdAndUpdate(req.params.projectID, {
+            title,
+            description
+        }, { new: true });
+
+        project.tasks = [];
+        await Task.deleteMany({ project: project._id });
+
+        await Promise.all(tasks.map(async task => {
+            const projectTask = new Task({ ...task, project: project._id });
+
+            await projectTask.save();
+
+            project.tasks.push(projectTask);
+        }));
+
+        await project.save();
+
+        return res.send({ project });
+    } catch (err) {
+        return res.status(400).send({ error: 'Error updating project'});
+    }
 });
 
 //delete route
